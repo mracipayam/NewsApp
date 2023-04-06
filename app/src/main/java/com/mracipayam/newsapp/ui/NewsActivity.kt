70f13ac012a119005b90ff6bb26066e839977604
Murat Han Acıpayam <acipayammurathan@gmail.com>
2023-04-07 01:20:51 +0300

    Bottom navgiation menu working

--- app/src/main/java/com/mracipayam/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/mracipayam/newsapp/ui/NewsActivity.kt
@@ -2,12 +2,27 @@ package com.mracipayam.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.mracipayam.newsapp.R
+import com.mracipayam.newsapp.databinding.ActivityNewsBinding
 
 class NewsActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityNewsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+//        setContentView(R.layout.activity_news)
+        val navController = binding.newsNavHostFragment.getFragment<NavHostFragment>().navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+
     }
 
     //https://newsapi.org/v2/everything?q=tesla&from=2023-03-06&sortBy=publishedAt&apiKey=25000c3d31184555900f727ea06959f7
