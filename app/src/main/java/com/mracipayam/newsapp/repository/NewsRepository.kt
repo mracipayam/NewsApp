fcd66889fa5b85acc913a63a2a6f2fc8e915aa2b
Murat Han Acıpayam <acipayammurathan@gmail.com>
2023-04-26 22:06:04 +0300

    Breaking news bind adapter

--- app/src/main/java/com/mracipayam/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/mracipayam/newsapp/repository/NewsRepository.kt
@@ -1,9 +1,11 @@
 package com.mracipayam.newsapp.repository
 
+import com.mracipayam.newsapp.api.RetrofitInstance
 import com.mracipayam.newsapp.db.ArticleDatabase
 
 class NewsRepository(
     val db: ArticleDatabase
-
 ) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
 }
--- app/src/main/java/com/mracipayam/newsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/mracipayam/newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -1,20 +1,83 @@
 package com.mracipayam.newsapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mracipayam.newsapp.R
+import com.mracipayam.newsapp.adapters.NewsAdapter
+import com.mracipayam.newsapp.databinding.FragmentBreakingNewsBinding
 import com.mracipayam.newsapp.ui.NewsActivity
 import com.mracipayam.newsapp.ui.NewsViewModel
+import com.mracipayam.newsapp.util.Resource
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
+    val TAG = "BreakingNewsFragment"
+
+    private var _binding : FragmentBreakingNewsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBreakingNewsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         viewModel = (activity as NewsActivity).viewModel
+        setUpRecyclerView()
 
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer {response ->
+            when(response){
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG,"An error occured: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun hideProgressBar() {
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+    }
+    private fun showProgressBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setUpRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
